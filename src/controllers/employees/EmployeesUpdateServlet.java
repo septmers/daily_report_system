33b13e93fd7bb5a67abeca15648dc2e52e6d6f6b
nanami <sept.mers1004@gmail.com>
2019-11-16 13:42:09 +0900

    Add Employee Destroy

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -28,7 +28,7 @@ public class EmployeesUpdateServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String _token = request.getParameter("_token");
+        String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())){
             EntityManager em = DBUtil.createEntityManager();
 
