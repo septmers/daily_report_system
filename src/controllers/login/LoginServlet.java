c653fd0d5bea96145e93e35a91636d9f599554ee
nanami <sept.mers1004@gmail.com>
2019-11-21 20:30:38 +0900

    Modify LoginServlet

--- src/controllers/login/LoginServlet.java
+++ src/controllers/login/LoginServlet.java
@@ -74,7 +74,7 @@ public class LoginServlet extends HttpServlet {
             request.setAttribute("hasError", true);
             request.setAttribute("code", code);
 
-            RequestDispatcher rd = request.getRequestDispatcher("/WEB^INF/views/login/login.jsp");
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
             rd.forward(request, response);
 
         }else{
